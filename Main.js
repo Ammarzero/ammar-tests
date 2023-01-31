1465d8e7bd0a8e92508d856083cc8493a34655b9
Ammarzero <123972401+Ammarzero@users.noreply.github.com>
2023-01-31 19:01:47 +0200

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+// wait the window to load 
+window.onload = function () {
+
+//document.querySelector("h1").style.color='blue';
+document.querySelector("h1").style.fontSIZE='60px';
+}
+
+window.alert("Hello From Js File");
